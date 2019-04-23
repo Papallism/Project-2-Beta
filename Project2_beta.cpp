0edfadda5cd594fdf535d87a0c96d52e2b028e8b
Michalis Papallis <mihalispapallis@gmail.com>
2019-04-23 16:42:36 +0300

    Add files via upload
    Added menu function and empty switch in main

--- Project2_beta.cpp
+++ Project2_beta.cpp
@@ -2,6 +2,9 @@
 #include <cstring>
 using namespace std;
 
+const int MAX = 100;
+int currentCustomers = 0;
+
 struct Account{
     int number;
     int type;
@@ -16,7 +19,40 @@ struct Customer{
     int totalAccounts;
 };
 
+int menu();
+
 int main()
 {
+    int choice;
+    do
+    {
+        choice = menu();
+        switch(choice)
+        {
+            case 1: break;
+            case 2: break;
+            case 3: break;
+            case 4: break;
+            case 5: break;
+            case 6: break;
+            case 7: break;
+            case 8: break;
+            case 9: break;
+            default: cout << "\nGood bye!\n";
+                     break;
+        }
+    }while(choice != 10);
     return 0;
+}
+
+int menu()
+{
+    int choice;
+    do
+    {
+        cout << "\nOperations:\n1) Add a new customer\n2) Print current total of customers\n3) Find a customer\n4) Print account holder details\n5) Print customer details\n6) Open a new account\n7) Delete customer\n8) Withdraw amount\n9) Provide reports\n10) Exit\nPlease choose operation: ";
+        cin >> choice;
+        if(choice < 1 || choice > 10)
+            cout << "\nInvalid input!\n";
+    }while(choice < 1 || choice > 10);
 }
